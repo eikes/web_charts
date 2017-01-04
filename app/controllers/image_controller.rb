4ee337b63af88c8e01121f2154b0cad3aa647bcb
netznarkose <mmaintz@posteo.de>
2017-01-04 10:41:32 +0100

    remove post controller

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,13 +1,15 @@
 class ImageController < ApplicationController
+
   def new
   end
+
   def create_image
     data = params[:data]
     if data[:file_format].eql?('png')
       h = GraphTool::CircleCountGraph.new([data[:icon_number].to_i], type: :png)
       send_data h.render, type: 'image/png', disposition: 'inline'
     elsif data[:file_format].eql?('svg')
-      r = GraphTool::CircleCountGraph.new([[data[:icon_number].to_i])
+      r = GraphTool::CircleCountGraph.new([data[:icon_number].to_i])
       render text: r.render
     else
       raise
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   post 'image/create_image'
   get 'image/new'
   root 'image#new'
-
-  resources :posts
 end
