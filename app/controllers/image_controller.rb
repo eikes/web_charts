3f20a5d7a90fec8a806db14d8d0bf93e617ad787
netznarkose <mmaintz@posteo.de>
2017-01-04 12:43:51 +0100

    convert param data to symbols

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -4,15 +4,47 @@ class ImageController < ApplicationController
   end
 
   def create_image
-    data = params[:data]
-    if data[:file_format].eql?('png')
-      h = GraphTool::CircleCountGraph.new([data[:icon_number].to_i], type: :png)
+    data = params[:image_params][:data]
+    o = params[:image_params]
+    o.delete(:data)
+    b = o.symbolize_keys
+    options = b.each do |key, value| 
+      b[key] = value.to_sym if value.is_a? String
+    end
+    if options[:type].eql?(:png)
+      h = GraphTool::CircleCountGraph.new([22], options)
       send_data h.render, type: 'image/png', disposition: 'inline'
-    elsif data[:file_format].eql?('svg')
-      r = GraphTool::CircleCountGraph.new([data[:icon_number].to_i])
+    elsif options[:type].eql?(:svg)
+      r = GraphTool::CircleCountGraph.new([22], options)
       render text: r.render
     else
       raise
     end
   end
+
+  # def image_render(date_me)
+  #   if options[:file_format].eql?('png')
+  #     send_data h.render, type: 'image/png', disposition: 'inline'
+  #   elsif options[:file_format].eql?('svg')
+  #     render text: r.render
+  #   else
+  #     raise
+  #   end
+  # end
+
+  # def graph
+  #   if type == :txt
+  #     GraphTool::SymbolCountGraph.new(data, graph_options)
+  #   elsif [:svg, :png, :jpg, :gif].include? type
+  #     if style == :circle
+  #       GraphTool::CircleCountGraph.new(data, graph_options)
+  #     elsif style == :cross
+  #       GraphTool::CrossCountGraph.new(data, graph_options)
+  #     elsif style == :manikin
+  #       GraphTool::ManikinCountGraph.new(data, graph_options)
+  #     elsif style == :bar
+  #       GraphTool::BarChart.new(data, graph_options)
+  #     end
+  #   end
+  # end
 end
