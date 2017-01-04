2cb7d03e4a76d0940a7771c96845566cfd76e81e
netznarkose <mmaintz@posteo.de>
2017-01-04 12:56:25 +0100

    fix image_routing_spec

---
+++ spec/routing/images_routing_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe ImageController, type: :routing do
+  describe "routing" do
+
+    it "routes to #new" do
+      expect(:get => "/image/new").to route_to("image#new")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/image/create_image").to route_to("image#create_image")
+    end
+
+  end
+end
