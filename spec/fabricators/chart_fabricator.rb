609222a1bc82cf5f76be128445c737e802b3733d
netznarkose <mmaintz@posteo.de>
2017-01-06 20:50:21 +0100

    add fabricator for chart

--- spec/fabricators/chart_fabricator.rb
+++ spec/fabricators/chart_fabricator.rb
@@ -1,3 +1,12 @@
 Fabricator(:chart) do
-  title "MyString"
+  title 'fabrication'
+  background_color 'white'
+  columns '20'
+  grouplabels 'pizza, pasta, spaghetti'
+  height '1000'
+  width '1000'
+  item_height '50'
+  item_width '50'
+  file_type 'svg'
+  style 'circle'
 end
