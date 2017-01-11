39f5abe1834ae241cf02b4acae5b08158639f9f3
netznarkose <mmaintz@posteo.de>
2017-01-11 16:42:04 +0100

    improve fabricators chart and datum

--- spec/fabricators/chart_fabricator.rb
+++ spec/fabricators/chart_fabricator.rb
@@ -1,12 +1,13 @@
 Fabricator(:chart) do
   title 'fabrication'
   background_color 'white'
-  columns '20'
+  columns 20
   grouplabels 'pizza, pasta, spaghetti'
-  height '1000'
-  width '1000'
-  item_height '50'
-  item_width '50'
+  height 1000
+  width 1000
+  item_height 50
+  item_width 50
   file_type 'svg'
   style 'circle'
+  data count: 4
 end
--- spec/fabricators/datum_fabricator.rb
+++ spec/fabricators/datum_fabricator.rb
@@ -1,6 +1,5 @@
 Fabricator(:datum) do
-  value '10'
+  value 10
   color 'red'
   label 'fire'
-  chart_id '1'
 end
