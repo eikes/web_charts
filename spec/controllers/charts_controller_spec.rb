ca4dcfa3c9cfabd1d443500df01eca3c2aeb883d
netznarkose <mmaintz@posteo.de>
2017-01-13 13:30:53 +0100

    rubocop corrections on charts_controller_spec

--- spec/controllers/charts_controller_spec.rb
+++ spec/controllers/charts_controller_spec.rb
@@ -1,24 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ChartsController, type: :controller do
-
   let!(:chart) { Fabricate(:chart) }
   let(:valid_attributes) { Fabricate.attributes_for(:chart) }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    skip('Add a hash of attributes invalid for your model')
   }
 
-
-
-  describe "GET #show" do
-    it "assigns the requested chart as @chart" do
-      get :show, { id: chart.to_param }
+  describe 'GET #show' do
+    it 'assigns the requested chart as @chart' do
+      get :show, id: chart.to_param
       expect(assigns(:chart)).to eq(chart)
     end
     context 'svg' do
-      it "returns svg" do
-        get :show, { id: chart.to_param }
+      it 'returns svg' do
+        get :show, id: chart.to_param
         expect(response.body).to match(/svg/)
       end
     end
@@ -26,66 +23,64 @@ RSpec.describe ChartsController, type: :controller do
       before do
         chart.update(file_type: 'png')
       end
-      it "returns svg" do
-        get :show, { id: chart.to_param }
+      it 'returns successfull status code when png-image is required' do
+        get :show, id: chart.to_param
         expect(response).to be_success
       end
     end
-
   end
 
-  describe "GET #new" do
-    it "assigns a new chart as @chart" do
+  describe 'GET #new' do
+    it 'assigns a new chart as @chart' do
       get :new, {}
       expect(assigns(:chart)).to be_a_new(Chart)
     end
   end
 
-  describe "GET #edit" do
-    it "assigns the requested post as @post" do
-      get :edit, { id: chart.to_param }
+  describe 'GET #edit' do
+    it 'assigns the requested post as @post' do
+      get :edit, id: chart.to_param
       expect(assigns(:chart)).to eq(chart)
     end
   end
 
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Chart" do
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Chart' do
         expect {
-          post :create, {chart: valid_attributes }
+          post :create, chart: valid_attributes
         }.to change(Chart, :count).by(1)
       end
 
-      it "assigns a newly created chart as @chart" do
-        post :create, {chart: valid_attributes }
+      it 'assigns a newly created chart as @chart' do
+        post :create, chart: valid_attributes
         expect(assigns(:chart)).to be_a(Chart)
         expect(assigns(:chart)).to be_persisted
       end
 
-      it "redirects to the created chart" do
-        post :create, {chart: valid_attributes }
+      it 'redirects to the created chart' do
+        post :create, chart: valid_attributes
         expect(response).to redirect_to(Chart.last)
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved chart as @chart" do
-        post :create, {chart: invalid_attributes }
+    context 'with invalid params' do
+      it 'assigns a newly created but unsaved chart as @chart' do
+        post :create, chart: invalid_attributes
         expect(assigns(:chart)).to be_a_new(Chart)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {chart: invalid_attributes }
-        expect(response).to render_template("new")
+        post :create, chart: invalid_attributes
+        expect(response).to render_template('new')
       end
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      it "updates the requested chart" do
-        put :update, { id: chart.to_param, chart: { title: 'fabrication_clone' } }
+  describe 'PUT #update' do
+    context 'with valid params' do
+      it 'updates the requested chart' do
+        put :update, id: chart.to_param, chart: { title: 'fabrication_clone' }
         chart.reload
         expect(chart.title).to eq('fabrication_clone')
       end
@@ -126,5 +121,4 @@ RSpec.describe ChartsController, type: :controller do
   #     expect(response).to redirect_to(charts_url)
   #   end
   # end
-
 end
