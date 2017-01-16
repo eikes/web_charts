af04b54f4c20419cf028f3207309a5dc0e883db4
netznarkose <mmaintz@posteo.de>
2017-01-16 16:14:17 +0100

    validate presence of value in datum model

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,3 +1,4 @@
 class Datum < ActiveRecord::Base
   belongs_to :chart
+  validates :value, presence: true
 end
--- spec/models/datum_spec.rb
+++ spec/models/datum_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Datum, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:datum) { Fabricate(:datum, value: 1, label: 'fire', color: 'red') }
+
+  describe 'validations' do
+    it 'creates a new instance of a user given valid attributes' do
+      expect(datum).to be_valid
+    end
+
+    it 'is invalid without data' do
+      datum.value = nil
+      expect(datum).not_to be_valid
+    end
+  end
 end
