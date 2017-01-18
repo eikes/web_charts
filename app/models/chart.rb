27b167c6ff58f977ff98e4166c10724dc3280d6d
netznarkose <mmaintz@posteo.de>
2017-01-18 11:44:53 +0100

    add init_defaults method to model/chart.rb
    background_color, file_type and style now get set after object
    initialization

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -3,6 +3,13 @@ class Chart < ActiveRecord::Base
   accepts_nested_attributes_for :data, reject_if: :all_blank, allow_destroy: true
   validates :data, presence: true
 
+  after_initialize :init_defaults, :if => :new_record?
+  def init_defaults
+    self.background_color = '#ffffff'
+    self.style = :circle
+    self.file_type = :svg
+  end
+
   def gem_params
     {
       title:            title,
